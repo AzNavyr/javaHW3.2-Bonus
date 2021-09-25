96a5cb04c06d69806730bc24dbc4db03eaf11ffc
Osmanov Enver <87546226+AzNavyr@users.noreply.github.com>
2021-09-25 23:57:48 +0300

    Update and rename BonusTerar.java to BonusTenar.java

--- BonusTenar.java
+++ BonusTenar.java
@@ -2,10 +2,10 @@ public class BonusTerar {
     public static void main(String[] args) {
         int score = 100;
         int topup = 100;
-        int TotalScore =  score + topup;
+        int totalScore =  score + topup;
         boolean condition = topup >= 1000;
 
-        int bonus = (condition ? topup / 100 + TotalScore : TotalScore);
+        int bonus = (condition ? topup / 100 + totalScore : totalScore);
         System.out.println(bonus);
     }
 }
