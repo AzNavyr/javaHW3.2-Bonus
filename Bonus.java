229bd5bfe51e3a83734fc3bf630942a73d44ab6b
Osmanov Enver <87546226+AzNavyr@users.noreply.github.com>
2021-09-25 23:56:53 +0300

    Update Bonus.java

--- Bonus.java
+++ Bonus.java
@@ -2,12 +2,12 @@ public class Bonus {
     public static void main(String[] args) {
         int score = 100;
         int topup = 100;
-        int TotalScore =  score + topup;
+        int totalScore =  score + topup;
 
         int bonus;
         if (topup >= 1000) {
-            bonus = topup / 100 + TotalScore;
-        } else {bonus = TotalScore; }
+            bonus = topup / 100 + totalScore;
+        } else {bonus = totalScore; }
         System.out.println(bonus);
         }
     }
