d28dc0716f067eb28e250db3a9e448c365749993
Osmanov Enver <cesar-88@mail.ru>
2021-09-26 21:05:04 +0300

    ChangedFiles

--- Bonus.java
+++ Bonus.java
@@ -2,13 +2,15 @@ public class Bonus {
     public static void main(String[] args) {
         int score = 100;
         int topup = 100;
-        int TotalScore =  score + topup;
+        int totalScore = score + topup;
 
         int bonus;
         if (topup >= 1000) {
-            bonus = topup / 100 + TotalScore;
-        } else {bonus = TotalScore; }
-        System.out.println(bonus);
+            bonus = topup / 100 + totalScore;
+        } else {
+            bonus = totalScore;
         }
+        System.out.println(bonus);
     }
+}
 
